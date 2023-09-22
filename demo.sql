cbcb0a94339fb1edf40990a603e3b040c623b2e6
Jesper Grip <jesper.grip@snowflake.com>
2023-09-22 10:41:39 +0200

    Clean up demo.sql

--- demo.sql
+++ demo.sql
@@ -45,10 +45,3 @@ CREATE OR REPLACE SECRET git_secret
     HANDLER = 'filter.filter_by_role';
 
   CALL filter_by_role('employees', 'dev');
-
-
-from snowflake.snowpark.functions import col
-
-def filter_by_role(session, table_name, role):
-  df = session.table(table_name)
-  return df.filter(col("role") == role)
